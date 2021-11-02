235d60d2f0b929b154d73e0868cd30a521f4ec6c
Tt6588 <tatsusato1011@gmail.com>
2021-11-02 09:20:03 +0900

    [finish]j-2

--- 2/app.js
+++ 2/app.js
@@ -13,19 +13,19 @@ if(orange < apple){
 }
 
 let max = 100;
-let num = 1;
+let num1 = 1;
 let count = 0;
-while(num < max){
-  num = num * 2;
+while(num1 < max){
+  num1 = num1 * 2;
   count = count + 1;
 }
 alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
 let i;
-let num = 0;
+let num2 = 0;
 
 for(i = 1; i < 11; i++){
-  num = num + i;
+  num2 = num2 + i;
 }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+alert('1から10まで足し算した結果は' + num2 + 'です');
